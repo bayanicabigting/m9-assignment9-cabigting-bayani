77154dc9d11770b879e20dd62e331c5f073e180f
Bayani Cabigting <noodlenavi@gmail.com>
2026-04-05 22:56:43 -0400

    updated index.js

--- index.js
+++ index.js
@@ -8,11 +8,39 @@ const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnVa
 
 const makeTag = tag => str => `<${tag}>${str}</${tag}>`
 
-// complete this function
-const makePoemHTML = () => {}
+const makePoemHTML = poemData => {
+  const poem = poemData[0]
 
-// attach a click event to #get-poem
+  const makeH2 = makeTag('h2')
+  const makeH3 = makeTag('h3')
+  const makeP = makeTag('p')
+  const makeEM = makeTag('em')
+
+  const makeAuthor = pipe(
+    name => "by " + name,
+    makeEM,
+    makeH3
+  )
+  
+  const paragraphs = poem.lines
+    .join("\n")
+    .split("\n\n")
+    .map(stanza =>
+      makeP(
+        stanza
+          .split("\n")
+          .join("<br>")
+      )
+    )
+    .join("")
+  
+  return (
+    makeH2(poem.title) +
+    makeAuthor(poem.author) +
+    paragraphs
+  )
+}
+
 getPoemBtn.onclick = async function() {
-  // renders the HTML string returned by makePoemHTML to #poem
   poemEl.innerHTML = makePoemHTML(await getJSON(poemURL))
 }
